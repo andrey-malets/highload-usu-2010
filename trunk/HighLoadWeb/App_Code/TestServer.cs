c6b701eca70723a93fa413906d3bdb73e1de861b
tagaychinova@gmail.com <tagaychinova@gmail.com@7f0eaa80-821d-31e6-e83f-e92f56c39101>
2010-04-07 20:04:44 +0000

    Web интерфейс

---
+++ trunk/HighLoadWeb/App_Code/TestServer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Common;
+
+/// <summary>
+/// Summary description for TestServer
+/// </summary>
+public class TestServer
+{
+    private static List<HighLoadDataString> allData;
+
+    static TestServer()
+    {
+        allData = new List<HighLoadDataString>();
+        allData.Add(new HighLoadDataString(1, "abstract"));
+        allData.Add(new HighLoadDataString(2, "event"));
+        allData.Add(new HighLoadDataString(3, "new"));
+        allData.Add(new HighLoadDataString(4, "struct"));
+        allData.Add(new HighLoadDataString(5, "as"));
+        allData.Add(new HighLoadDataString(6, "explicit"));
+        allData.Add(new HighLoadDataString(7, "null"));
+        allData.Add(new HighLoadDataString(8, "switch"));
+        allData.Add(new HighLoadDataString(9, "base"));
+        allData.Add(new HighLoadDataString(10, "extern"));
+        allData.Add(new HighLoadDataString(11, "object"));
+        allData.Add(new HighLoadDataString(12, "this"));
+        allData.Add(new HighLoadDataString(13, "bool"));
+        allData.Add(new HighLoadDataString(14, "false"));
+        allData.Add(new HighLoadDataString(15, "operator"));
+        allData.Add(new HighLoadDataString(16, "throw"));
+        allData.Add(new HighLoadDataString(17, "break"));
+        allData.Add(new HighLoadDataString(18, "finally"));
+        allData.Add(new HighLoadDataString(19, "out"));
+    }
+
+    public static List<HighLoadDataString> GetAllData()
+    {
+        return allData;
+    }
+
+    private static HighLoadDataString getHighLoadDataString(Guid id)
+    {
+        return allData.Where<HighLoadDataString>(c => c.Id == id)
+            .Take<HighLoadDataString>(1).ToList<HighLoadDataString>()[0];
+    }
+
+    public static void DeleteData(Guid id)
+    {
+        allData.Remove(getHighLoadDataString(id));
+    }
+
+    public static void AddData(HighLoadDataString data)
+    {
+        allData.Add(data);
+    }
+
+    public static void UpdateData(Guid id, int number, string name)
+    {
+        HighLoadDataString highLoadDataString = getHighLoadDataString(id);
+        highLoadDataString.Number = number;
+        highLoadDataString.Name = name;
+    }
+}
---
+++ trunk/HighLoadWeb/Default.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Common;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            GridViewData.DataSource = TestServer.GetAllData();
+            GridViewData.DataBind();
+        }
+    }
+
+    protected void DetailsViewData_ItemCreated(object sender, EventArgs e)
+    {
+        if (DetailsViewData.FooterRow != null)
+        {
+            int commandRowIndex = DetailsViewData.Rows.Count - 1;
+            DetailsViewRow commandRow = DetailsViewData.Rows[commandRowIndex];
+
+            DataControlFieldCell cell = (DataControlFieldCell)commandRow.Controls[0];
+            foreach (Control ctl in cell.Controls)
+            {
+                LinkButton link = ctl as LinkButton;
+                if (link != null)
+                {
+                    if (link.CommandName == "Delete")
+                    {
+                        link.ToolTip = "Click here to delete";
+                        link.OnClientClick = "return confirm('Do you really want to delete this record?');";
+                    }
+                    else if (link.CommandName == "New")
+                    {
+                        link.ToolTip = "Click here to add a new record";
+                    }
+                    else if (link.CommandName == "Edit")
+                    {
+                        link.ToolTip = "Click here to edit the current record";
+                    }
+                }
+            }
+        }
+    }
+
+    protected void HighLoadGridView_PageIndexChanging(Object sender, GridViewPageEventArgs e)
+    {
+        GridViewData.DataSource = TestServer.GetAllData();
+        GridViewData.PageIndex = e.NewPageIndex;
+        GridViewData.DataBind();
+    }
+
+    protected void HighLoadDetailsView_PageIndexChanging(Object sender, DetailsViewPageEventArgs e)
+    {
+        DetailsViewData.DataSource = TestServer.GetAllData();
+        DetailsViewData.PageIndex = e.NewPageIndex + GridViewData.PageSize * GridViewData.PageIndex;
+        DetailsViewData.DataBind();
+    }
+
+    protected void HighLoadGridView_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        DetailsViewData.ChangeMode(DetailsViewMode.ReadOnly);
+        DetailsViewData.DataSource = TestServer.GetAllData();
+        DetailsViewData.PageIndex = GridViewData.SelectedIndex + GridViewData.PageSize * GridViewData.PageIndex;
+        DetailsViewData.DataBind();
+    }
+
+    protected void HighLoadDetailsView_ModeChanging(object sender, DetailsViewModeEventArgs e)
+    {
+        DetailsViewData.ChangeMode(DetailsViewMode.Edit);
+        DetailsViewData.DataSource = TestServer.GetAllData();
+        DetailsViewData.PageIndex = GridViewData.SelectedIndex;
+        DetailsViewData.DataBind();
+    }
+
+    protected void HighLoadDetailsView_ItemDeleting(object sender, DetailsViewDeleteEventArgs e)
+    {
+        TestServer.DeleteData(new Guid(GridViewData.SelectedDataKey.Value.ToString()));
+        DetailsViewData.ChangeMode(DetailsViewMode.Insert);
+        GridViewData.DataSource = TestServer.GetAllData();
+        GridViewData.SelectedIndex = -1;
+        GridViewData.DataBind();
+    }
+
+    protected void HighLoadDetailsView_ItemUpdating(object sender, DetailsViewUpdateEventArgs e)
+    {
+        Guid id = new Guid(GridViewData.SelectedDataKey.Value.ToString());
+        int number = int.Parse(((TextBox)DetailsViewData.Rows[1].Controls[1].Controls[0]).Text);
+        string name = ((TextBox)DetailsViewData.Rows[2].Controls[1].Controls[0]).Text;
+        TestServer.UpdateData(id, number, name);
+        DetailsViewData.ChangeMode(DetailsViewMode.ReadOnly);
+        List<HighLoadDataString> allData = TestServer.GetAllData();
+        DetailsViewData.DataSource = allData;
+        DetailsViewData.DataBind();
+        GridViewData.DataSource = allData;
+        GridViewData.DataBind();
+    }
+
+    protected void HighLoadDetailsView_ItemInserting(object sender, DetailsViewInsertEventArgs e)
+    {
+        TestServer.AddData(getHighLoadDataString());
+        List<HighLoadDataString> allData = TestServer.GetAllData();
+        DetailsViewData.DataSource = allData;
+        DetailsViewData.DataBind();
+        GridViewData.DataSource = allData;
+        GridViewData.DataBind();
+    }
+
+    private HighLoadDataString getHighLoadDataString()
+    {
+        //string id = ((TextBox)DetailsViewData.Rows[0].Controls[1].Controls[0]).Text;
+        int number = int.Parse(((TextBox)DetailsViewData.Rows[1].Controls[1].Controls[0]).Text);
+        string name = ((TextBox)DetailsViewData.Rows[2].Controls[1].Controls[0]).Text;
+        return new HighLoadDataString(number, name);
+    }
+}
+
