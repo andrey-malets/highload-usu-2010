cd6879fd53730b4e909e4b2ec9e4073af9393da2
tagaychinova@gmail.com <tagaychinova@gmail.com@7f0eaa80-821d-31e6-e83f-e92f56c39101>
2010-04-07 20:04:44 +0000

    Web интерфейс

---
+++ Common/HighLoadDataString.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Common
+{
+    // что-то примерное, для теста HighLoadWeb
+    public class HighLoadDataString
+    {
+        private Guid id;
+
+        public Guid Id
+        {
+            get { return id; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int number;
+
+        public int Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        private DateTime revisionTime;
+
+        public DateTime RevisionTime
+        {
+            get { return revisionTime; }
+        }
+
+        public HighLoadDataString(int number, string name)
+        {
+            this.id = System.Guid.NewGuid();
+            this.number = number;
+            this.name = name;
+            this.revisionTime = DateTime.Now;
+        }
+    }
+}
