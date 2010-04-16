42f51bfef4913e3b8288ea358a4bc701512bffc7
tagaychinova@gmail.com <tagaychinova@gmail.com@7f0eaa80-821d-31e6-e83f-e92f56c39101>
2010-04-16 12:03:26 +0000

    переделан элемент управления выводящий список всех данных

---
+++ trunk/HighLoad/HighLoadWeb/Controls/DetailsViewData.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="~/Controls/DetailsViewData.ascx.cs" Inherits="Controls.DetailsViewData" %>
+<asp:TextBox ID="Example" runat="server" />
---
+++ trunk/HighLoad/HighLoadWeb/Controls/DetailsViewData.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Controls
+{
+    public partial class DetailsViewData : UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ViewElement(Guid id)
+        {
+            Example.Text = id.ToString();
+        }
+    }
+}
