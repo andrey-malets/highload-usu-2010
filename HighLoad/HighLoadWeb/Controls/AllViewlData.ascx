eb4e8c8247c9411949263a45193adbb1552e5619
tagaychinova@gmail.com <tagaychinova@gmail.com@7f0eaa80-821d-31e6-e83f-e92f56c39101>
2010-04-16 12:03:26 +0000

    переделан элемент управления выводящий список всех данных

---
+++ HighLoad/HighLoadWeb/Controls/AllViewlData.ascx
@@ -0,0 +1,44 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="~/Controls/AllViewlData.ascx.cs" Inherits="Controls.AllViewlData" %>
+
+<asp:Label ID="Empty" runat="server" Visible="false" Text="There's no data to show in this view." />
+<asp:Repeater ID="ViewAllData" runat="server" OnItemDataBound="ViewAllData_ItemDataBound">
+    <HeaderTemplate>
+<table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tr class="title">
+        <th class="view"></th>
+        <th class="view">Number</th>
+        <th class="view">Name</th>
+        <th class="view">Revision Time</th>
+    </tr>
+    </HeaderTemplate>
+    <ItemTemplate>
+    <tr>
+        <td class="view"><asp:LinkButton ID="SelectLink" runat="server" 
+            Text="select" OnClick="SelectLink_Click" CommandArgument=<%# Eval("Id")%> />
+        </td>
+        <td class="view"><%# Eval("Number")%></td>
+        <td class="view"><%# Eval("Name")%></td>
+        <td class="view"><%# Eval("RevisionTime")%></td>
+    </tr>
+    </ItemTemplate>
+    <FooterTemplate>
+        <asp:Repeater ID="ViewPage" runat="server" OnItemCommand="ViewPage_ItemCommand">
+            <HeaderTemplate>
+    <tr class="title">
+        <td colspan="4" class="view" align="center">            
+            </HeaderTemplate>
+            <ItemTemplate>
+                <asp:LinkButton ID="PageLink" runat="server" ForeColor="White" 
+                    Font-Underline = "<%# Container.DataItem.ToString() != ((PageNumber + 1).ToString()) %>"
+                    CommandArgument="<%# Container.DataItem %>">
+                    <%# Container.DataItem %>
+                </asp:LinkButton> 
+            </ItemTemplate>
+            <FooterTemplate>
+        </td>
+    </tr>          
+            </FooterTemplate>
+        </asp:Repeater>
+</table>
+    </FooterTemplate>
+</asp:Repeater> 
---
+++ HighLoad/HighLoadWeb/Controls/AllViewlData.ascx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Web.UI.WebControls;
+using App_Code;
+
+namespace Controls
+{
+    public partial class AllViewlData : System.Web.UI.UserControl
+    {
+        public delegate void SelectHandler(Guid id);       
+        public event SelectHandler Select;
+
+        private const int PageSize = 10;
+
+        public int PageNumber
+        {
+            get
+            {
+                if (ViewState["PageNumber"] != null)
+                    return Convert.ToInt32(ViewState["PageNumber"]);
+                return 0;
+            }
+            set
+            {
+                ViewState["PageNumber"] = value;
+            }
+        }
+
+        protected void ViewPage_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            PageNumber = Convert.ToInt32(e.CommandArgument) - 1;
+            LoadData();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+                LoadData();
+        }
+
+        private void LoadData()
+        {
+            var allData = TestServer.GetAllData();
+            if (allData.Count == 0)
+            {
+                ViewAllData.Visible = false;
+                Empty.Visible = true;
+            }
+            else
+            {
+                var pgitems = new PagedDataSource
+                                  {
+                                      DataSource = allData,
+                                      AllowPaging = true,
+                                      PageSize = PageSize,
+                                      CurrentPageIndex = PageNumber
+                                  };
+
+                ViewAllData.DataSource = pgitems;
+                ViewAllData.DataBind();
+            }
+        }
+
+        protected void SelectLink_Click(object sender, EventArgs e)
+        {
+            if (Select != null)
+            {
+                var selectIdButton = sender as LinkButton;
+                if (selectIdButton != null)
+                {
+                    string id = selectIdButton.CommandArgument;
+                    Select(new Guid(id));
+                }
+            }
+        }
+
+        public void ViewAllData_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            var pageItems = ViewAllData.DataSource as PagedDataSource;
+            if (pageItems != null)
+            {
+                var viewPage = e.Item.FindControl("ViewPage") as Repeater;
+                if (viewPage != null)
+                {
+                    if (pageItems.PageCount > 1)
+                    {
+                        var pages = new ArrayList();
+                        for (int i = 0; i < pageItems.PageCount; i++)
+                            pages.Add((i + 1).ToString());
+
+                        viewPage.DataSource = pages;
+                        viewPage.DataBind();
+                    }
+                    else
+                        viewPage.Visible = false;
+                }
+            }
+        }
+    }
+}
