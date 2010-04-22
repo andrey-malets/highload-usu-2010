9d96570c6d1133a4fed846d92bac831adbae9a0f
mvmalygin <mvmalygin@7f0eaa80-821d-31e6-e83f-e92f56c39101>
2010-04-22 02:38:43 +0000

    Обертка над NHibernate

---
+++ HighLoad/DataManager/EntityManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using USU.HighLoad.Storage.StorageDataContract;
+
+namespace USU.HighLoad.Storage.DataManager
+{
+    public class EntityManager
+    {
+        DataRecord record = new DataRecord();
+    }
+}
