a67cc798f78c26bfaad81a63ff6e38f00b4e2589
igor.kaf <igor.kaf@7f0eaa80-821d-31e6-e83f-e92f56c39101>
2010-05-05 19:27:14 +0000

    Добавлена реализация сервиса работы с хранилищем на основе HttpListener

---
+++ trunk/HighLoad/ServiceHost/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using Common;
+using StorageService;
+
+namespace ServiceHost
+{
+    class Program
+    {
+        class TestDataSerializer : IDataSerializer
+        {
+            private readonly BinaryFormatter _binaryFormatter;
+
+            public TestDataSerializer()
+            {
+                _binaryFormatter = new BinaryFormatter();
+            }
+
+            public byte[] Serialize<T>( T param )
+            {
+                var mem = new MemoryStream();
+                _binaryFormatter.Serialize( mem, param );
+                return mem.ToArray();
+            }
+
+            public T Deserialize<T>( byte[] bytes )
+            {
+                return (T)_binaryFormatter.Deserialize( new MemoryStream( bytes ) );
+            }
+        }
+
+        static void Main( string[] args )
+        {
+            var endpoints = new[] {new ServerEndpoint {Port = "8001", Url = "http://localhost"}};
+
+            using ( var service = new DataService( endpoints, new TestDataSerializer() ) )
+            {
+                try
+                {
+                    service.Open();
+                }
+                catch ( Exception ex )
+                {
+                    Console.Write( ex.Message );
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
