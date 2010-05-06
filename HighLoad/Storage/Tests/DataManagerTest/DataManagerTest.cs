be7ff0d94a6d8d1ce01aa1707959209882fb3d79
mvmalygin <mvmalygin@7f0eaa80-821d-31e6-e83f-e92f56c39101>
2010-05-06 04:23:24 +0000

    Тесты к DataManager

---
+++ HighLoad/Storage/Tests/DataManagerTest/DataManagerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using NUnit.Framework;
+using USU.HighLoad.Storage.StorageDataContract;
+
+namespace DataManagerTests
+{
+    [TestFixture]
+    public class DataManagerTest
+    {
+        [Test]
+        public void CanGenerateSchema()
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(DataRecord).Assembly);
+
+            new SchemaExport(cfg).Execute(false, true, false);
+        }
+    }
+}
