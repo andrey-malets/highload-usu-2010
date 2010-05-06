51715a4624270c372f72988fc9e5005a61f3a29c
mvmalygin <mvmalygin@7f0eaa80-821d-31e6-e83f-e92f56c39101>
2010-05-06 04:23:24 +0000

    Тесты к DataManager

--- trunk/HighLoad/Storage/DataManager/EntityManager.cs
+++ trunk/HighLoad/Storage/DataManager/EntityManager.cs
@@ -3,11 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using USU.HighLoad.Storage.StorageDataContract;
+using Common.DataContracts;
 
 namespace USU.HighLoad.Storage.DataManager
 {
-    public class EntityManager
+    public class EntityManager<T> : IDataManager<T> where T : class, IStoragable, new()
     {
-        DataRecord record = new DataRecord();
+        #region Члены IDataManager<T>
+
+        public List<T> LoadAll()
+        {
+            return new List<T>();
+        }
+
+        public T Load(Guid id)
+        {
+            return new T();
+        }
+
+        public void Save(T record)
+        {
+        }
+
+        public void Update(T record)
+        {
+        }
+
+        #endregion
     }
 }
---
+++ trunk/HighLoad/Storage/DataManager/IDataManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common.DataContracts;
+
+namespace USU.HighLoad.Storage.DataManager
+{
+    public interface IDataManager<T> where T : IStoragable
+    {
+        List<T> LoadAll();
+
+        T Load(Guid id);
+
+        void Save(T record);
+
+        void Update(T record);
+    }
+}
